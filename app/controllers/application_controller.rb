77d0724cf749a47526d260567dab1db39f3c82f6
mikiodesu <212180654+mikiodesu@users.noreply.github.com>
2025-08-07 13:57:43 +0000

    DEVISE 実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+
+  
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_one_attached :image
+  validates :name,  length: { maximum: 12 }
+  validates :introduction, length: { maximum: 100 }
+end
