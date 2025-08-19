26ebb192007d7e99e2820dc0d9fcc1c3c3acae41
mikiodesu <212180654+mikiodesu@users.noreply.github.com>
2025-08-19 15:48:55 +0000

    likes,comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,13 @@
 class LikesController < ApplicationController
   def create
+    like = current_user.likes.new(workout_id: params[:workout_id])
+    like.save
+    redirect_to request.referer
   end
 
   def destroy
+    like = current_user.likes.find_by(workout_id: params[:workout_id])
+    like.destroy
+    redirect_to request.referer
   end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,4 +4,9 @@ class Workout < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :workout_details, allow_destroy: true
+
+   
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
